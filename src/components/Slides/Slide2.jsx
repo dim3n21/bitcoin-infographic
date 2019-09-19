bcbf52d54deccab5ce83101d37cc932808875777
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-09-19 15:26:44 -0700

    Update the Slide2. Add titles to circles.

--- src/components/Slides/Slide2.jsx
+++ src/components/Slides/Slide2.jsx
@@ -8,28 +8,56 @@ class Slide2 extends Component {
       constructor() {
             super();
             this.state = {
-              percent: 0,
+              percent1: 0,
+              percent2: 0,
+              percent3: 0,
             };
             this.increase = this.increase.bind(this);
+            this.increase2 = this.increase2.bind(this);
+            this.increase3 = this.increase3.bind(this);
           }  
       
           componentDidMount() {
             this.increase();
+            this.increase2();
+            this.increase3();
           }
         
           increase() {
-            const { percent } = this.state;
-            const newPercent = percent + 1;
-            if (newPercent >= 65) {
+            const { percent1 } = this.state;
+            const newPercent = percent1 + 1;
+            if (newPercent >= 85) {
               clearTimeout(this.tm);
               return;
             }
-            this.setState({ percent: newPercent });
-            this.tm = setTimeout(this.increase, 10);
+            this.setState({ percent1: newPercent });
+            this.tm = setTimeout(this.increase, 1);
+          }
+
+          increase2() {
+            const { percent2 } = this.state;
+            const newPercent2 = percent2 + 1;
+            if (newPercent2 >= 95) {
+              clearTimeout(this.tm);
+              return;
+            }
+            this.setState({ percent2: newPercent2 });
+            this.tm = setTimeout(this.increase2, 15);
+          }
+
+          increase3() {
+            const { percent3 } = this.state;
+            const newPercent3 = percent3 + 1;
+            if (newPercent3 >= 53) {
+              clearTimeout(this.tm);
+              return;
+            }
+            this.setState({ percent3: newPercent3 });
+            this.tm = setTimeout(this.increase3, 20);
           }
       
       render () {
-            const { percent } = this.state;
+            const { percent1, percent2, percent3 } = this.state;
             return (
                   <div className="audience">
                        <Row>
@@ -45,27 +73,35 @@ class Slide2 extends Component {
                                                 <Col xxl={12} sm={12} xs={12} style={{padding: '1rem'}} align="center">
                                                 <div className="pcircle">
                                                       <div className="pcircle_percentage">
-                                                            {percent}                     
+                                                            <span style={{
+                                                                  display: 'block',
+                                                                  fontSize: '1rem'
+                                                            }}>B2B</span>
+                                                            {`${percent1}%`}                     
                                                       </div>
                                                       <Circle
                                                             strokeWidth="7"
                                                             strokeColor="#95D2BA"
                                                             trailWidth="7"
                                                             trailColor="#FDD982"
-                                                            percent={percent} />         
+                                                            percent={percent1} />         
                                                 </div>
                                                 </Col>
                                                 <Col xxl={12} sm={12} xs={12} style={{padding: '1rem'}} align="center">
                                                 <div className="pcircle">
                                                       <div className="pcircle_percentage">
-                                                            {percent}                     
+                                                      <span style={{
+                                                                  display: 'block',
+                                                                  fontSize: '1rem'
+                                                            }}>B2C</span>
+                                                            {`${percent2}%`}                     
                                                       </div>
                                                       <Circle
                                                             strokeWidth="7"
                                                             strokeColor="#95D2BA"
                                                             trailWidth="7"
                                                             trailColor="#FDD982"
-                                                            percent={percent} />         
+                                                            percent={percent2} />         
                                                 </div>
                                                 </Col>
                                           </Row>
@@ -73,14 +109,18 @@ class Slide2 extends Component {
                                                 <Col xxl={24} sm={24} xs={24} style={{padding: '1rem'}} align="center">
                                                 <div className="pcircle">
                                                       <div className="pcircle_percentage">
-                                                            {percent}                     
+                                                      <span style={{
+                                                                  display: 'block',
+                                                                  fontSize: '1rem'
+                                                            }}>Customers</span>
+                                                            {`${percent3}%`}                     
                                                       </div>
                                                       <Circle
                                                             strokeWidth="7"
                                                             strokeColor="#95D2BA"
                                                             trailWidth="7"
                                                             trailColor="#FDD982"
-                                                            percent={percent} />         
+                                                            percent={percent3} />         
                                                 </div>
                                                 </Col>
                                           </Row>
